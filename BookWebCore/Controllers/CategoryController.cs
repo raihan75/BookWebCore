5cc37e38c52ce30fa63ee07b0d318fdccd52e909
raihan75 <raihanabedin75@gmail.com>
2024-04-04 14:34:18 +0600

    BookWebCore

---
+++ BookWebCore/Controllers/CategoryController.cs
@@ -0,0 +1,33 @@
+﻿using BookWebCore.Data;
+using BookWebCore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookWebCore.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDBContex _db;
+        public CategoryController(ApplicationDBContex db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCatoryList = _db.Categories.ToList();
+            return View(objCatoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ BookWebCore/Data/ApplicationDBContex.cs
@@ -0,0 +1,25 @@
+﻿using BookWebCore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookWebCore.Data
+{
+    public class ApplicationDBContex : DbContext
+    {
+        public ApplicationDBContex(DbContextOptions<ApplicationDBContex> options) :base(options) 
+        {
+            
+        }
+
+        public DbSet<Category> Categories{ get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+
+                );
+        }
+    }
+}
